f2a002282ca8d6a8828636d46a88328953ab22d3
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-03 16:28:22 +0300

    create image model, make migrarion

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'home')
+    image_name = models.CharField(max_length = 50)
+    image_caption = models.TextField(max_length = 50)
+    # profile = models.ForeignKey(Profile)
+    likes = models.CharField(max_length = 25)
+    comments = models.TextField(max_length = 144)
