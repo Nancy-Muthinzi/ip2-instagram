11845db4c7deaec272ebf2c10e6083bd8ef7e7ce
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-03 12:35:24 +0300

    install bootstrap3

--- instagram/views.py
+++ instagram/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def login(request):
-    return HttpResponse('Sign Up and Log In to Instagram')
+    return render(request,'login.html')
