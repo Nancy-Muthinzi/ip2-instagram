12ecb9f5f6ecfc95411396b97137f4a0bbf55ac5
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-04 12:59:38 +0300

    create a logout functionality

--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 from .models import Image
 from django.contrib.auth.decorators import login_required
 from .forms import CommentForm
