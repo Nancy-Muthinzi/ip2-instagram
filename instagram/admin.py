df8464b4d053cb093ac237c602a890aa756c5d7f
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-05 10:31:43 +0300

    create .env file for email config, add to .gitignore

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import UserProfile,Image
+from .models import UserProfile,Image, Tag, Comment,Like
 
 admin.site.register(UserProfile)
 admin.site.register(Image)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(Like)
