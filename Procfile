36c55486025155c67e4606e997c2c6e8adfaff49
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-07 21:40:32 +0300

    prepare app for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ip2.wsgi
--- ip2/settings.py
+++ ip2/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'instagram.apps.InstagramConfig',
     'bootstrap3',
     'registration',
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
     'django_forms_bootstrap',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -46,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,13 +128,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestSta
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# configuring the location for media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
 
 # Email configurations
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
